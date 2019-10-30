5853e0ba4b26701262ed28493d8b60fbd4d1e3bb
mihiro <f.mihiroxx@gmail.com>
2019-10-30 10:27:21 +0900

    Add dialdb(), clisedb() to main.go

--- twitterbotes/main.go
+++ twitterbotes/main.go
@@ -1,3 +1,17 @@
 package main
 
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイヤル中： localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
 func() {}
