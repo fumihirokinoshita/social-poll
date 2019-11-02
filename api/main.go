e80629d1e4867e7495663220a9fad17119194555
mihiro <f.mihiroxx@gmail.com>
2019-11-02 19:49:40 +0900

    Add withAPIKey() to main.go

--- api/main.go
+++ api/main.go
@@ -1,3 +1,15 @@
 package main
 
+import "net/http"
+
 func main() {}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r, URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIです")
+			return
+		}
+		fn(w, r)
+	}
+}
