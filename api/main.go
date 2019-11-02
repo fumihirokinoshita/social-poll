c964a44717cd4c54a2c6b952473e5f082423a9f7
mihiro <f.mihiroxx@gmail.com>
2019-11-02 19:54:09 +0900

    Add withVars() to main.go

--- api/main.go
+++ api/main.go
@@ -17,3 +17,11 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 func isValidAPIKey(key string) bool {
 	return key == "abc123"
 }
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
